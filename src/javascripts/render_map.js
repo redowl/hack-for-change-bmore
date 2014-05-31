9a6aa1ef222a149759821f5826b9c092e99dc34e
Chris Poirel <chris.poirel@gmail.com>
2014-05-31 14:03:02 -0400

    working prototype

--- src/javascripts/render_map.js
+++ src/javascripts/render_map.js
@@ -2,8 +2,8 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 7,
+    center: new google.maps.LatLng(39, -76)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
