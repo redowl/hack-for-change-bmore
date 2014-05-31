dd54b59cb1b90992f69bf97b4bfda56d09257f42
Chris Poirel <chris.poirel@gmail.com>
2014-05-31 16:36:56 -0400

    adding pins to the graphs

--- src/javascripts/render_map.js
+++ src/javascripts/render_map.js
@@ -1,40 +1,23 @@
 var map;
-
-// <script type="text/javascript" src="javascripts/markers.js"></script>
-
-var locations = [
-    { name: "Talbot County Career Center",
-      street: "301 Bay Street",
-      city: "Easton",
-      zip: "21601",
-      lat: 38.778,
-      long: -76.083
-    },
-    { name: "My house",
-      street: "1825 Aliceanna Street",
-      city: "Baltimore",
-      zip: "21231",
-      lat: 39.000,
-      long: -75.000
-    },
-
-];
+var url = "http://data.maryland.gov/resource/qswz-v9jx.json";
 
 function initialize() {
   var mapOptions = {
     zoom: 7,
-    center: new google.maps.LatLng(39, -76)
+    center: new google.maps.LatLng(39, -76.5)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  locations.forEach( function (location)
-  {
-      var myLatlng = new google.maps.LatLng(location.lat, location.long);
+  $.getJSON(url, function(data) {
+    $.each(data, function(i, row) {
+      console.log(row);
+      var myLatlng = new google.maps.LatLng(row.location_1.latitude, row.location_1.longitude);
       var marker = new google.maps.Marker({
         position: myLatlng,
         map: map,
-        title: location.name
+        title: row.description
       });
+    });
   });
 }
 
