7e305f2bbd677a4040f1be20a19363eba0e5ea24
GrimmOutlook <grimmoutlook87@gmail.com>
2014-05-31 16:01:25 -0400

    Added markers, jquery

---
+++ src/javascripts/markers.js
@@ -0,0 +1,23 @@
+var locations = [
+    { "name": "Talbot County Career Center"
+      "street": "301 Bay Street",
+      "city": "Easton",
+      "zip": "21601",
+      "lat": -76.083,
+      "long": 38.778
+    },
+    { "name": "My house"
+      "street": "1825 Aliceanna Street",
+      "city": "Baltimore",
+      "zip": "21231",
+      "lat": -75.000,
+      "long": 38.000
+    },
+    { "name": "Washington County One-Stop Shop Center"
+      "street": "14 N. Potomac Street",
+      "city": "Hagerstown",
+      "zip": "21740",
+      "lat": -77.719,
+      "long": 39.642
+    }
+];
--- src/javascripts/render_map.js
+++ src/javascripts/render_map.js
@@ -1,15 +1,24 @@
 var map;
 
+// <script type="text/javascript" src="javascripts/markers.js"></script>
+
 var locations = [
-    { "name": "slkjafl fskj df",
-      "lat": 67,
-      "lon": 34
+    { name: "Talbot County Career Center",
+      street: "301 Bay Street",
+      city: "Easton",
+      zip: "21601",
+      lat: 38.778,
+      long: -76.083
     },
-    { "name": "slkjafl fskj df",
-      "lat": 67,
-      "lon": 34
-    }
-]
+    { name: "My house",
+      street: "1825 Aliceanna Street",
+      city: "Baltimore",
+      zip: "21231",
+      lat: 39.000,
+      long: -75.000
+    },
+
+];
 
 function initialize() {
   var mapOptions = {
@@ -17,6 +26,16 @@ function initialize() {
     center: new google.maps.LatLng(39, -76)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  locations.forEach( function (location)
+  {
+      var myLatlng = new google.maps.LatLng(location.lat, location.long);
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: location.name
+      });
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
